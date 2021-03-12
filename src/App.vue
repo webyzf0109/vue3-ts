658d11a640b72b8ad5c122ac0a39042a4404dd66
webyzf0109@163.com <webyzf0109@163.com>
2021-03-12 16:44:06 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/mine">mine</router-link> |
+    <router-link to="/profile">profile</router-link>
   </div>
   <router-view/>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home/index.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,12 +8,14 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/mine',
+    name: 'Mine',
+    component: () => import(/* webpackChunkName: "mine" */ '../views/mine/index.vue')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: () => import(/* webpackChunkName: "profile" */ '../views/profile/index.vue')
   }
 ]
 
